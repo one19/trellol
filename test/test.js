bfe6ce0ee44c5907710c9103d5ebe96d08fab459
Drew Showalter <maelstroswim@gmail.com>
2016-06-26 22:31:40 +1000

    Fixes test notation for instantiating selenium/mocha tests.

--- test/test.js
+++ test/test.js
@@ -7,12 +7,16 @@ const app = require('../web');
 
 const service = new chrome.ServiceBuilder(path).build();
 chrome.setDefaultService(service);
-const driver = new webdriver.Builder()
+const makeWebDriver = () => {
+  const driver = new webdriver.Builder()
     .withCapabilities(webdriver.Capabilities.chrome())
     .build();
+  return driver;
+};
 
 test.describe('Google Search', () => {
   test.it('should work', () => {
+    const driver = makeWebDriver();
     driver.get('http://www.google.com');
     const searchBox = driver.findElement(webdriver.By.name('q'));
     searchBox.sendKeys('simple programmer');
@@ -26,13 +30,9 @@ test.describe('Google Search', () => {
 
 test.describe('Trellol', () => {
   test.it('Should load on default port.', () => {
-    const web = app();
+    const web = app.listen();
+    const driver = makeWebDriver();
     driver.get('https://localhost:3002');
-    const searchBox = driver.findElement(webdriver.By.name('q'));
-    searchBox.sendKeys('simple programmer');
-    searchBox.getAttribute('value').then((value) => {
-      assert.equal(value, 'simple programmer');
-    });
     driver.quit();
   });
 });
